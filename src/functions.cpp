8658c63a5682e8c32155d4f873a30d61448d1f09
gf777 <giulio.formenti@gmail.com>
2023-01-14 22:49:31 -0800

    test

--- src/functions.cpp
+++ src/functions.cpp
@@ -512,6 +512,7 @@ void computeNstars(std::vector<unsigned long long int>& lens, // compute N/L* st
 
 void rmChrFromStr(std::string &str, const char* charsToRemove) {
    for (unsigned int i = 0; i < strlen(charsToRemove); ++i ) {
+       std::cout<<"hello"<<std::endl;
       str.erase(std::remove(str.begin(), str.end(), charsToRemove[i]), str.end());
    }
 }
