c79ba51059d3d8c9b57c5fd956beda90d2372a83
gf777 <giulio.formenti@gmail.com>
2023-03-01 21:07:21 -0500

    generalized to multiple files

--- include/fastx.h
+++ include/fastx.h
@@ -9,107 +9,104 @@ bool loadSequences(UserInput userInput, OBJECT* object, char type, unsigned int*
     std::string firstLine, newLine, seqHeader, seqComment, line, bedHeader;
     unsigned int seqPos = 0; // to keep track of the original sequence order
     unsigned int batchSize = 10000; // number of sequences processed by a thread
-        
-    if (!userInput.iSeqFileArg.empty() || userInput.pipeType == type) {
                 
-        //stream objects
-        StreamObj streamObj;
-        std::shared_ptr<std::istream> stream;
-        stream = streamObj.openStream(userInput, type, fileNum);
+    //stream objects
+    StreamObj streamObj;
+    std::shared_ptr<std::istream> stream;
+    stream = streamObj.openStream(userInput, type, fileNum);
+    
+    if (stream) {
         
-        if (stream) {
-            
-            switch (stream->peek()) {
+        switch (stream->peek()) {
+                
+            case '>': {
+                
+                stream->get();
+                
+                while (getline(*stream, newLine)) {
                     
-                case '>': {
+                    seqHeader = std::string(strtok(strdup(newLine.c_str())," ")); //process header line
+                    c = strtok(NULL,""); //read comment
                     
-                    stream->get();
+                    if (c != NULL)
+                        seqComment = std::string(c);
                     
-                    while (getline(*stream, newLine)) {
-                        
-                        seqHeader = std::string(strtok(strdup(newLine.c_str())," ")); //process header line
-                        c = strtok(NULL,""); //read comment
-                        
-                        if (c != NULL)
-                            seqComment = std::string(c);
-                        
-                        std::string* inSequence = new std::string;
-                        getline(*stream, *inSequence, '>');
-                        lg.verbose("Individual fasta sequence read");
-                        Sequence* sequence = new Sequence{seqHeader, seqComment, inSequence, NULL};
-                        sequence->seqPos = seqPos; // remember the order
-                        object->appendSequence(sequence);
-                        seqPos++;
-                        
-                    }
-                    
-                    jobWait(threadPool);
-                    
-                    break;
+                    std::string* inSequence = new std::string;
+                    getline(*stream, *inSequence, '>');
+                    lg.verbose("Individual fasta sequence read");
+                    Sequence* sequence = new Sequence{seqHeader, seqComment, inSequence, NULL};
+                    sequence->seqPos = seqPos; // remember the order
+                    object->appendSequence(sequence);
+                    seqPos++;
                     
                 }
+                
+                jobWait(threadPool);
+                
+                break;
+                
+            }
+                
+            case '@': {
+                
+                Sequences* readBatch = new Sequences;
+
+                while (getline(*stream, newLine)) { // file input
+
+                    newLine.erase(0, 1);
+                    seqHeader = std::string(strtok(strdup(newLine.c_str())," ")); //process header line
+                    c = strtok(NULL,""); //read comment
                     
-                case '@': {
+                    if (c != NULL)
+                        seqComment = std::string(c);
+
+                    std::string* inSequence = new std::string;
+                    getline(*stream, *inSequence);
+                    getline(*stream, newLine);
+                    ignore(*stream, '\n');
+                    readBatch->sequences.push_back(new Sequence {seqHeader, seqComment, inSequence});
+                    seqPos++;
+
+                    lg.verbose("Individual fastq sequence read: " + seqHeader);
                     
-                    Sequences* readBatch = new Sequences;
+                    if (seqPos % batchSize == 0 || stream->peek() == EOF) {
 
-                    while (getline(*stream, newLine)) { // file input
+                        readBatch->batchN = seqPos/batchSize + 1;
+                        lg.verbose("Processing batch N: " + std::to_string(readBatch->batchN));
 
-                        newLine.erase(0, 1);
-                        seqHeader = std::string(strtok(strdup(newLine.c_str())," ")); //process header line
-                        c = strtok(NULL,""); //read comment
-                        
-                        if (c != NULL)
-                            seqComment = std::string(c);
-
-                        std::string* inSequence = new std::string;
-                        getline(*stream, *inSequence);
-                        getline(*stream, newLine);
-                        ignore(*stream, '\n');
-                        readBatch->sequences.push_back(new Sequence {seqHeader, seqComment, inSequence});
-                        seqPos++;
-
-                        lg.verbose("Individual fastq sequence read: " + seqHeader);
-                        
-                        if (seqPos % batchSize == 0 || stream->peek() == EOF) {
-
-                            readBatch->batchN = seqPos/batchSize + 1;
-                            lg.verbose("Processing batch N: " + std::to_string(readBatch->batchN));
-
-                            threadPool.queueJob([=]{ return object->traverseInReads(readBatch); });
-                            std::unique_lock<std::mutex> lck(mtx);
-                            for (auto it = object->logs.begin(); it != object->logs.end(); it++) {
-                             
-                                it->print();
-                                object->logs.erase(it--);
-                                
-                            }
+                        threadPool.queueJob([=]{ return object->traverseInReads(readBatch); });
+                        std::unique_lock<std::mutex> lck(mtx);
+                        for (auto it = object->logs.begin(); it != object->logs.end(); it++) {
+                         
+                            it->print();
+                            object->logs.erase(it--);
                             
-                            if (stream->peek() != EOF)
-                                readBatch = new Sequences;
-
                         }
+                        
+                        if (stream->peek() != EOF)
+                            readBatch = new Sequences;
 
                     }
 
-                    break;
-
                 }
-                    
+
+                break;
+
             }
+                
+        }
+        
+        //consolidate log
+        jobWait(threadPool);
+        for (auto it = object->logs.begin(); it != object->logs.end(); it++) {
             
-            //consolidate log
-            jobWait(threadPool);
-            for (auto it = object->logs.begin(); it != object->logs.end(); it++) {
-                
-                it->print();
-                object->logs.erase(it--);
-                
-            }
+            it->print();
+            object->logs.erase(it--);
             
         }
         
     }
+
     
     return true;
 
