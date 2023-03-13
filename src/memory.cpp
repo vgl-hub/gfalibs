900a53e34c91aa9c385b6f103b08cb997a70a31a
gf777 <giulio.formenti@gmail.com>
2023-03-13 15:30:00 -0400

    handle windows

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,11 +1,22 @@
+#ifndef _WIN32
 
 #include <sys/resource.h>
 
+#endif
+
 unsigned long int get_mem_usage(){
+    
+#ifndef _WIN32
     
     struct rusage thisUsage;
     
     getrusage(RUSAGE_SELF, &thisUsage);
     return thisUsage.ru_maxrss;
+    
+#else
+    
+    return 0;
+    
+#endif
     
 }
