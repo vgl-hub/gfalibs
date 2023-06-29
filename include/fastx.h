d229be1bacf94e5a7376cc143af43c05ec6f9ad8
gf777 <giulio.formenti@gmail.com>
2023-06-29 07:24:12 -0400

    consolidate after n iter

--- include/fastx.h
+++ include/fastx.h
@@ -133,7 +133,7 @@ template<typename OBJECT>
 bool loadKmers(UserInput userInput, OBJECT* object, char type, unsigned int* fileNum) { // load from FASTA/FASTQ to templated object, faster when we only need to retain kmers not the original reads
     
     // stream read variables
-    int batchSize = 10000000; // number of bases processed by a thread
+    int batchSize = 10000000, iter = 0; // number of bases processed by a thread
                 
     //stream objects
     StreamObj streamObj;
@@ -173,7 +173,13 @@ bool loadKmers(UserInput userInput, OBJECT* object, char type, unsigned int* fil
                         }
                     }
                     
-                    object->consolidate();
+                    if (iter == 10) {
+                        
+                        object->consolidate();
+                        iter = 0;
+                    }
+                    
+                    ++iter;
 
                 }
 
