d25c213ac5fe73de9e59f9f11307a3662360f109
gf777 <giulio.formenti@gmail.com>
2024-02-24 14:20:42 -0500

    a string to graph walking struct

--- include/node-graph.h
+++ include/node-graph.h
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
 struct GraphNode {
     
-    char base;
+    uint8_t base;
     std::vector<GraphNode*> next;
     
-    GraphNode(char base) : base(base) {}
+    GraphNode(uint8_t base) : base(base) {}
     
     void addNext(GraphNode* nextNode) {
         next.push_back(nextNode);
@@ -15,62 +14,112 @@ struct GraphNode {
     
 };
 
-void deleteStringGraph(GraphNode *graphNode) {
+struct StringGraph {
     
-    if (graphNode->next.size() > 0)
-        deleteStringGraph(graphNode->next[0]);
-    delete graphNode;
-
-}
-
-std::vector<std::string> walkStringGraph(GraphNode *graphNode, std::string currentPath){
+    GraphNode *root = new GraphNode(5);
+    std::vector<GraphNode*> leaves;
+    uint8_t *seq;
+    uint8_t k;
+    uint64_t pos = k-1;
+    GraphNode *prev;
     
-    std::vector<std::string> paths;
-    if (graphNode->base != 'J' && graphNode->base != '0' )
-        currentPath+=graphNode->base;
     
-    for (GraphNode *nextGraphNode : graphNode->next) {
-        if (nextGraphNode->next.size() == 0) {
-            paths.push_back(currentPath+=nextGraphNode->base);
-            break;
+    StringGraph(uint8_t *seq, uint8_t k) : seq(seq), k(k) {
+        
+        GraphNode *prev = root;
+        
+        for (uint8_t pos = 0; pos < k-1; ++pos) {
+            GraphNode *graphNode = new GraphNode(seq[pos]);
+            prev->addNext(graphNode);
+            this->prev = prev;
+            prev = graphNode;
         }
-        std::vector<std::string> discoveredPaths = walkStringGraph(nextGraphNode, currentPath);
-        paths.insert(paths.end(), discoveredPaths.begin(), discoveredPaths.end());
+        leaves.push_back(prev);
+
     }
-    return paths;
-}
 
-std::vector<std::string> stringToGraph(std::string seq, uint64_t err, uint8_t k) {
+    uint8_t peek() {
+        
+        return seq[pos];
+        
+    }
     
-    GraphNode *root = new GraphNode('0');
-    GraphNode *prev = root;
+    void pop_back() {
+        
+        if (root->next.size() > 0) {
+            std::vector<GraphNode*> tmp = root->next[0]->next;
+            for (GraphNode* nextNode : root->next)
+                delete nextNode;
+            root->next = tmp;
+        }
+    }
     
-    for (uint64_t i = 0; i < seq.size()-k+1; ++i) {
-        //std::cout<<seq.substr(i, k)<<std::endl;
-        if (i+k-1 == err) {
+    void appendNode() {
+        
+        GraphNode* newLeaf = new GraphNode(seq[pos++]);
+        for (GraphNode* leaf : leaves)
+            leaf->addNext(newLeaf);
+        leaves.clear();
+        leaves.push_back(newLeaf);
+        this->prev = newLeaf;
             
-            for (uint8_t pos = i+1; pos < i+k-1; ++pos) {
-                GraphNode *graphNode = new GraphNode(seq[pos]);
-                prev->addNext(graphNode);
-                prev = graphNode;
-            }
-
-            GraphNode *pri = new GraphNode(seq[i+k-1]);
-            prev->addNext(pri);
-            GraphNode *alt = new GraphNode('A');
-            prev->addNext(alt);
-            GraphNode *alt2 = new GraphNode('J');
-            prev->addNext(alt2);
-            GraphNode *next = new GraphNode(seq[i+k]);
-            pri->addNext(next);
-            alt->addNext(next);
-            alt2->addNext(next);
-            prev = next;
+    }
+    
+    void addAlt(std::vector<uint8_t> alts) {
+        
+        leaves.clear();
+        
+        for (uint8_t alt : alts) {
+            
+            GraphNode *newLeaf = new GraphNode(alt);
+            this->prev->addNext(newLeaf);
+            leaves.push_back(newLeaf);
             
         }
         
+        ++pos;
+        pop_back();
+            
     }
-    std::vector<std::string> paths = walkStringGraph(root, std::string());
-    deleteStringGraph(root);
-    return paths;
-}
+    
+    void addIns(uint8_t altBase) {
+        
+
+    }
+    
+    std::vector<std::vector<uint8_t>> walkStringGraph(GraphNode *graphNode, std::vector<uint8_t> currentPath){
+        
+        std::vector<std::vector<uint8_t>> paths;
+        if (graphNode->base != 4 && graphNode->base != 5 )
+            currentPath.push_back(graphNode->base);
+        
+        for (GraphNode *nextGraphNode : graphNode->next) {
+            if (nextGraphNode->next.size() == 0) {
+                currentPath.push_back(nextGraphNode->base);
+                currentPath.push_back(seq[pos]);
+                currentPath.push_back(seq[pos+1]);
+                paths.push_back(currentPath);
+                break;
+            }
+            std::vector<std::vector<uint8_t>> discoveredPaths = walkStringGraph(nextGraphNode, currentPath);
+            paths.insert(paths.end(), discoveredPaths.begin(), discoveredPaths.end());
+        }
+        return paths;
+    }
+    
+    void deleteStringGraph(GraphNode *graphNode) {
+        
+        if (graphNode->next.size() > 0)
+            deleteStringGraph(graphNode->next[0]);
+        for (uint8_t n = 1; n < graphNode->next.size(); ++n)
+            delete graphNode->next[n];
+        
+        delete graphNode;
+
+    }
+    
+    ~StringGraph() {
+        deleteStringGraph(root);
+    }
+    
+};
