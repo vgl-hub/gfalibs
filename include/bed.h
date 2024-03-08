e8c07d6152d3268ec45d49edf8805dd28f4b8c56
gf777 <giulio.formenti@gmail.com>
2024-03-08 11:35:40 -0500

    extended BedCoordinates to use hashing for random access

--- include/bed.h
+++ include/bed.h
@@ -4,42 +4,57 @@
 #include <vector>
 #include <string>
 
+#include "parallel-hashmap/phmap.h"
+
 class BedCoordinates { // generic representation of bed coordinates
 private:
-    std::vector<std::string> seqHeaders;
-    std::vector<unsigned int> cBegin;
-    std::vector<unsigned int> cEnd;
+    
+    phmap::flat_hash_map<std::string,std::vector<std::pair<uint64_t,uint64_t>>> coordinates;
     
 public:
     
-    void pushCoordinates(std::string h, unsigned int b = 0, unsigned int e = 0) { // reading coordinates
-        seqHeaders.push_back(h);
-        cBegin.push_back(b);
-        cEnd.push_back(e);
+    void pushCoordinates(std::string h, uint64_t s = 0, uint64_t e = 0) { // reading coordinates
+        
+        std::pair<uint64_t,uint64_t> newCoordinates(s,e);
+        auto got = coordinates.find(h); // insert or find this kmer in the hash table
+        
+        if (got == coordinates.end()){
+            std::vector<std::pair<uint64_t,uint64_t>> newCoordinatesVector = {newCoordinates};
+            std::pair<std::string,std::vector<std::pair<uint64_t,uint64_t>>> newCoordinatesVectorPair = std::make_pair(h,newCoordinatesVector);
+            coordinates.insert(newCoordinatesVectorPair);
+        }else{
+            got->second.push_back(newCoordinates);
+        }
     }
     
     inline bool empty() { // check if no coordinates are present
-        return (seqHeaders.size()==0) ? true : false; // check if no coordinates are present
+        return (coordinates.size()==0) ? true : false; // check if no coordinates are present
     }
     
-    inline unsigned int size(){
-        return seqHeaders.size(); // check if no coordinates are present
+    inline uint32_t size() { // check if no coordinates are present
+        return coordinates.size(); // check if no coordinates are present
     }
     
     inline std::vector<std::string> getSeqHeaders() { // get all the headers
+        std::vector<std::string> seqHeaders;
+        
+        for (std::pair<std::string,std::vector<std::pair<uint64_t,uint64_t>>> h: coordinates)
+            seqHeaders.push_back(h.first);
+        
         return seqHeaders;
     }
     
-    inline std::string getSeqHeader(unsigned int pos) { // get a specific header
-        return seqHeaders[pos];
+    inline bool isPresent(std::string h) { // get a specific header
+        auto got = coordinates.find(h); // insert or find this kmer in the hash table
+        
+        if (got == coordinates.end())
+            return false;
+        else
+            return true;
     }
     
-    inline unsigned int getcBegin(unsigned int pos) { // get a specific start coordinate
-        return cBegin[pos];
-    }
-    
-    inline unsigned int getcEnd(unsigned int pos) { // get a specific end coordinate
-        return cEnd[pos];
+    inline phmap::flat_hash_map<std::string,std::vector<std::pair<uint64_t,uint64_t>>>& getCoordinates() { // get a specific start coordinate
+        return coordinates; // insert or find this kmer in the hash table
     }
     
 };
